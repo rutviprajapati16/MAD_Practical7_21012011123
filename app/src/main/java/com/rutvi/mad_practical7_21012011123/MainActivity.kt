857efcab6c4d4c96a8b33df01a869cb1a8b525a3
RUTVI <rutvi162004@gmail.com>
2023-09-17 21:14:02 +0530

    Practical7

---
+++ app/src/main/java/com/rutvi/mad_practical7_21012011123/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.rutvi.mad_practical7_21012011123
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebBackForwardList
+import android.widget.Button
+import android.widget.MediaController
+import android.widget.VideoView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val mediaController = MediaController(this)
+        val uri: Uri= Uri.parse("android.resource://" + packageName + "/" + R.raw.thestoryoflight)
+        val myVideoView = findViewById<VideoView>(R.id.videoView)
+        myVideoView.setMediaController(mediaController)
+        mediaController.setAnchorView(myVideoView)
+        myVideoView.setVideoURI(uri)
+        myVideoView.requestFocus()
+        myVideoView.start()
+        val button: FloatingActionButton = findViewById(R.id.floatingActionButton3)
+        button.setOnClickListener(){
+            Intent(this@MainActivity,Youtube::class.java).also { startActivity(it) }
+        }
+    }
+}
---
+++ app/src/main/java/com/rutvi/mad_practical7_21012011123/Youtube.kt
@@ -0,0 +1,19 @@
+package com.rutvi.mad_practical7_21012011123
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+
+class Youtube : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube)
+        val youtubeId = "fJn9B64Znrk"
+        val youWebView = findViewById<WebView>(R.id.webview)
+        val webSettings : WebSettings = youWebView.settings
+        webSettings.javaScriptEnabled=true
+        webSettings.loadWithOverviewMode=true
+        youWebView.loadUrl("https://www.youtube.com/embed/$youtubeId")
+    }
+}
